1c48756d383a2d3496e6412fdc8fa011dd7fdf2b
Patrick Biffle <pbiffle@gmail.com>
2018-03-22 13:57:36 -0600

    testing twitter call function

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ var params = {
 }
 
 app.get('/', (request, response, next) => {
-	twitClient.get('search/tweets', params, (error, tweets, twitterResponse) => {
+	twitClient.get('search/tweets', params, (error, tweets, twitterResponse)).then(resp => {
 		if (error) {
 			next(error)
 		} else {
